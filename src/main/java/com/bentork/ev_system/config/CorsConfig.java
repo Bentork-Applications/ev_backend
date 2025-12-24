ffe27c88ce54a62e7fcaf682033bc2ee633f13d0
jayesh <backendintern01.bentork@gmail.com>
2025-12-24 14:11:25 +0530

    updated url

--- src/main/java/com/bentork/ev_system/config/CorsConfig.java
+++ src/main/java/com/bentork/ev_system/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://dev.bentork.in",
+                        .allowedOrigins("https://web.bentork.in",
                                 "http://localhost:5173") // React frontend
 
                         // (Vite)
